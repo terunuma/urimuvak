3c5b6d7ce1d2870d5f0c1aac04bd55e1a9e8f79c
fracmode <amunu.rets@gmail.com>
2016-08-14 21:23:51 +0900

    move procedure into MainClass

--- main.rb
+++ main.rb
@@ -1,23 +1,34 @@
 # TODO:
 #   Yahoo ファイナンスからスクレイピング
-#   http://stocks.finance.yahoo.co.jp/stocks/search/?s=%(terms)s&p=%(page)s&ei=UTF-8
+#     http://stocks.finance.yahoo.co.jp/stocks/search/?s=%(terms)s&p=%(page)s&ei=UTF-8
+#   Nomura
+#     http://quote.nomura.co.jp/nomura/cgi-bin/parser.pl?QCODE=1&TEMPLATE=nomura_tp_kabu_01
 
 # URLにアクセスするためのライブラリの読み込み
 require 'open-uri'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
 
-# スクレイピング先のURL
-url = 'http://www.yahoo.co.jp/'
 
-charset = nil
-html = open(url) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
+class MainClass
+	def scrapeOnePage
+		# スクレイピング先のURL
+		url = 'http://quote.nomura.co.jp/nomura/cgi-bin/parser.pl?QCODE=1&TEMPLATE=nomura_tp_kabu_01'
+
+		charset = nil
+		html = open(url) do |f|
+		  charset = f.charset # 文字種別を取得
+		  f.read # htmlを読み込んで変数htmlに渡す
+		end
+
+		# htmlをパース(解析)してオブジェクトを生成
+		doc = Nokogiri::HTML.parse(html, nil, charset)
+
+		# タイトルを表示
+		p doc.title
+
+	end
 end
 
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
-# タイトルを表示
-p doc.title
+mainClass = MainClass.new()
+mainClass.scrapeOnePage()
